8ecf5c102d9b1693bbc830e585d993c328ff0a58
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-01-31 13:17:55 -0800

    Enabling user-deletion and update
    * adding update, edit, and destroy actions to UsersController
    * adding delete button to rows in index template
    * adding edit template
    * updating integration tests

--- spec/create_user_spec.rb
+++ spec/create_user_spec.rb
@@ -10,21 +10,40 @@ describe 'Creating a user' do
   let(:timestamp) { Time.now.to_i.to_s }
   let(:full_name) { "Erin (#{timestamp})" }
 
-  def create_user
+  before 'creating a user' do
     visit '/'
-
     click_on 'Create new user'
 
     fill_in 'Full name', with: full_name
     fill_in 'Email', with: "erin+#{timestamp}@carbonfive.com"
     fill_in 'Phone', with: "#{rand(9999999999)}"
-
     click_on 'Save User'
-  end
-
-  it 'allows new user creation' do
-    create_user
-
     expect(page).to have_content("Successfully created user with name #{full_name}")
   end
+
+  before 'allows for editing a user' do
+    visit '/'
+    click_on "#{full_name}"
+
+    new_email = "erin+#{timestamp}+2@carbonfive.com"
+    fill_in 'Email', with: new_email
+    click_on 'Save User'
+    expect(page).to have_content("Successfully edited user with name #{full_name}")
+
+    visit '/'
+    expect(page).to have_content(new_email)
+  end
+
+
+  before 'allows for deletion of a user' do
+    visit '/'
+    find(:xpath, "//td[contains(.//a/text(), '#{full_name}')]").parent().find('.delete').click()
+    expect(page).to have_content("Successfully deleted user with name #{full_name}")
+  end
+
+  it 'completes the user lifecycle' do
+    visit '/'
+    expect(page).to_not have_content(full_name)
+  end
+
 end
